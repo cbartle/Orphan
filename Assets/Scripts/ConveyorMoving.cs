6d2a3516ea9b32b25e61f1f48ebce5c4dc54557a
Collin Bartley <Collin@Collins-MacBook.local>
2016-09-21 02:06:58 -0400

    changed the placement of where certain scripts are, for my sake of mind

---
+++ Assets/Scripts/ConveyorMoving.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConveyorMoving : MonoBehaviour {
+
+	//float speed = 1f;
+	public float velocity = 0.5f;
+
+
+
+	void OnCollisionStay2D(Collision2D obj){
+
+		//when colliding with the player		
+		//if (obj.gameObject.name == "Bucky") {
+
+			Debug.Log ("We are touching the conveyor");
+			//reacts if the script is on so that we can turn off and on the conveyor effect
+			if (gameObject.GetComponent<ConveyorMoving> ().enabled)
+				
+				obj.gameObject.transform.Translate (velocity, 0.0f, 0.0f);
+
+		//}
+
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+
+}
---
+++ Assets/Scripts/GoSceneTwo.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GoSceneTwo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnCollisionStay2D(Collision2D obj){
+
+		if (obj.gameObject.name == "Bucky") {
+			Debug.Log ("The Door");
+			Debug.Log (Input.GetKeyUp ("e"));
+			//Debug.Log (num);
+			if(Input.GetKeyUp("e")){
+
+				SceneManager.LoadScene ("WorldOneSceneTwo");
+			}
+
+
+			//Debug.Log (Conveyor.GetComponent<ConveyorMoving>().enabled);
+		}
+
+	}
+
+	//void LoadScene("WorldOneSceneTwo"){
+//
+//	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	
+
+	}
+}
