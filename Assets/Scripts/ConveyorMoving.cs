75cb54389a091cf3c703cbf882ec1f7abaa9ae85
Collin L. Bartley <boloman94@gmail.com>
2017-03-09 22:02:33 -0500

    fixing merge issues

--- Assets/Scripts/ConveyorMoving.cs
+++ Assets/Scripts/ConveyorMoving.cs
@@ -10,14 +10,11 @@ public class ConveyorMoving : MonoBehaviour {
 	void OnTriggerStay2D(Collider2D obj){
 
 		//reacts if the script is on so that we can turn off and on the conveyor effect
-		if (gameObject.GetComponent<ConveyorMoving> ().enabled){
-
-			if(obj.gameObject.GetComponent<PlatformerCharacter2D>().m_FacingRight)	
+		if (gameObject.GetComponent<ConveyorMoving> ().enabled) {
+			if (obj.gameObject.GetComponent<PlatformerCharacter2D> ().m_FacingRight)
 				obj.gameObject.transform.Translate (speed, 0.0f, 0.0f);
-			else 
+			else
 				obj.gameObject.transform.Translate (-speed, 0.0f, 0.0f);
-		
-
 		}
 		
 	}
--- Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -7,7 +7,7 @@ public class PlatformerCharacter2D : MonoBehaviour
 {
 	[SerializeField] private float m_MaxSpeed = 10f;
 	// The fastest the player can travel in the x axis.
-    [SerializeField] private float m_JumpForce = 400f;
+    [SerializeField] public float m_JumpForce = 400f;
 	// Amount of force added when the player jumps.
     [Range (0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;
 	// Amount of maxSpeed applied to crouching movement. 1 = 100%
